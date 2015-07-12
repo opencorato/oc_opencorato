1e1a9454c7955c38d93227f6bcd55c87dcdfadc7
root <root@vps179222.ovh.net>
2015-07-13 00:54:35 +0200

    align

--- openordini/oo_people/views.py
+++ openordini/oo_people/views.py
@@ -195,7 +195,7 @@ class OOPoliticianSearchView(PoliticianSearchView):
 
         # build persons array,substituting the img with a 50x50 thumbnail
         # and returning the absolute url of the thumbnail
-        print person.first_name
+        # print person.first_name
         
         results = []
         for person in persons:
