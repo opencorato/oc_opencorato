235a4b360431476c1016fe5531616aa0d8939626
idlweb <opencorato>
2015-07-26 12:23:50 +0200

    Errors on loading the profile's image

--- openordini/oo_users/views.py
+++ openordini/oo_users/views.py
@@ -353,7 +353,7 @@ class OOUserProfileEditView(FormView):
         except Exception, e: 
             subject = _(u"It was not possible to save the personal data of user: %(user)s") % { "user": smart_text(user) }
 
-        msg = _("%(subject)s.\n\nDetails: %(error)s\n\nProvided data:\n\n%(data)s") % { "subject": subject, "error": smart_text(e), "data": smart_text(log_data) }
+        msg = "%(subject)s.\n\nDetails: %(error)s\n\nProvided data:\n\n%(data)s" % { "subject": subject, "error": smart_text(e), "data": smart_text(log_data) }
 
         d = { "subject": subject }
 
